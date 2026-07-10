b86cc3da06c290be948aa2f485c760b61f253687
BoualamBillel <boualambillelsofien@gmail.com>
2026-07-10 16:49:52 +0200

    implemented etudiant dash that allow u to scan the formateur qr code to sign

--- importmap.php
+++ importmap.php
@@ -27,4 +27,5 @@ return [
     '@hotwired/stimulus' => ['version' => '3.2.2'],
     '@symfony/stimulus-bundle' => ['path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js'],
     '@hotwired/turbo' => ['version' => '8.0.23'],
+    'html5-qrcode' => ['version' => '2.3.8'],
 ];
